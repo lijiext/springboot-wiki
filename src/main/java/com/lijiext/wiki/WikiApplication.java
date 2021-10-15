d700def000a99f1f9ca543aa4209d042960276ac
Li Jie <lijiecn@vip.qq.com>
2021-10-15 21:29:53 +0800

    modify startup banner

--- src/main/java/com/lijiext/wiki/WikiApplication.java
+++ src/main/java/com/lijiext/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.lijiext.wiki;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+    public static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("应用启动成功！");
+        LOG.info("地址：\thttp://localhost:{}", environment.getProperty("server.port"));
     }
 
 }
