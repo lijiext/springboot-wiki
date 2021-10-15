348d334c071393ebe387005991e8e46c73fe95e2
Li Jie <lijiecn@vip.qq.com>
2021-10-15 22:47:09 +0800

    add project variable

--- src/main/java/com/lijiext/wiki/controller/TestController.java
+++ src/main/java/com/lijiext/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.lijiext.wiki.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.var:author_not_defined}")
+    private String author;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String hello(){
-        return "HELLO WORLD!";
+    public String hello() {
+        return "HELLO WORLD!\nThis project was made by " + author;
     }
 }
