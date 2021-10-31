eba31c954ef69bccc032bec70f820bd5057dec1f
OCTOPUSalive <842984716@qq.com>
2021-10-31 21:48:24 +0800

    集成mybatis-generator，自动生成demo表代码

--- doc/project.sql
+++ doc/project.sql
@@ -8,3 +8,11 @@ create table test (
 
 insert into test (id, name, password) values (1, 'xyp', '123456');
 insert into test (id, name, password) values (2, 'lj', '654321');
+
+drop table if exists demo;
+create table demo (
+                      id bigint not null,
+                      name varchar(50),
+                      primary key (id)
+) engine=innodb default charset=utf8mb4;
+insert into demo (id, name) values (1, 'xyp');
